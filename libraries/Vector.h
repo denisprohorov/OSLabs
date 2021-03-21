6fbfeaf98fc9c95479658cafa1f5696ce391dda8
Denis <denprohorov8@yandex.com>
2021-03-21 19:07:44 +0300

    linux work by wsl ubuntu

--- libraries/Vector.h
+++ libraries/Vector.h
@@ -1,27 +1,22 @@
 #pragma once
-#ifdef Vector_EXPORTS
-#define VECTOR_API __declspec(dllexport)
-#else
-#define VECTOR_API __declspec(dllimport)
-#endif
 
 #include <iostream>
 #include "Number.h"
 
-VECTOR_API class Vector{
+class Vector{
 private:
     Number x;
     Number y;
 
 public:
-    VECTOR_API Vector(int x, int y);
-    VECTOR_API Vector(Number x, Number y);
+    Vector(int x, int y);
+    Vector(Number x, Number y);
 
-    VECTOR_API std::string toString();
-    VECTOR_API Number r();
-	VECTOR_API Number fi();
+    std::string toString();
+    Number r();
+	Number fi();
 
-	VECTOR_API Vector operator+ (Vector& v);
+	Vector operator+ (Vector& v);
 
 };
 
