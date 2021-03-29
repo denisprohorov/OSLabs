d7bbd35ae141803d3926468ec6383d5971d3fdd3
denisprohorov <denprohorov8@yandex.com>
2021-03-29 19:39:30 +0300

    add condSend and condRecv in channel

--- buffered_channel.h
+++ buffered_channel.h
@@ -7,29 +7,30 @@
 
 template<class T>
 class BufferedChannel {
- private:
+private:
     std::queue<T> queue;
     const size_t size;
     std::mutex mutex;
-    std::condition_variable cond;
+    std::condition_variable condSend;
+    std::condition_variable condRecv;
     bool isOpen;
 
- public:
+public:
     explicit BufferedChannel(int size) : size(size), isOpen(true) {}
 
     void Send(T value){
         std::unique_lock<std::mutex> lock(this->mutex);
-        cond.wait(lock, [this](){return (queue.size() < this->size || !isOpen);});
+        condSend.wait(lock, [this](){return (queue.size() < this->size || !isOpen);});
 
         if(!isOpen) throw(std::runtime_error("Channel is closed, sorry"));
         queue.push(value);
         lock.unlock();
-        cond.notify_all();
+        condRecv.notify_one();
     }
 
     std::pair<T, bool> Recv() {
         std::unique_lock<std::mutex> lock(this->mutex);
-        cond.wait(lock, [this](){return (!this->queue.empty() || !isOpen);});
+        condRecv.wait(lock, [this](){return (!this->queue.empty() || !isOpen);});
         if(queue.empty()){
             std::pair<T, bool> pair;
             pair.second = false;
@@ -40,7 +41,7 @@ class BufferedChannel {
         std::pair<T, bool> pair(queue.front(), true);
         queue.pop();
         lock.unlock();
-        cond.notify_all();
+        condSend.notify_one();
         return pair;
     }
 
@@ -48,7 +49,8 @@ class BufferedChannel {
         std::unique_lock<std::mutex> lock(this->mutex);
         isOpen = false;
         lock.unlock();
-        cond.notify_all();
+        condRecv.notify_all();
+        condSend.notify_all();
     }
 };
 
